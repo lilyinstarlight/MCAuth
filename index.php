f6a4094884397b2d68fef83e3d617a913697568c
Foster McLane <fkmclane@gmail.com>
2013-04-15 14:55:20 -0400

    Updated magic number (Unix timestamp of current release)

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if(isset($_REQUEST['user']) && isset($_REQUEST['password'])) {
 		} while($result->num_rows != 0);
 		$mysql->query('UPDATE ' . $CONFIG['table'] . ' SET session="' . $id . '" WHERE id=' . $array['id']);
 
-		echo '1357737036000' . ':' . 'deprecated' . ':' . $array['username'] . ':' . $id . ':';
+		echo '1363862534000' . ':' . 'deprecated' . ':' . $array['username'] . ':' . $id . ':';
 	}
 	else if($CONFIG['onlineauth'] && isset($_REQUEST['version'])) {
 		echo file_get_contents('http://login.minecraft.net/?user=' . $_REQUEST['user'] . '&password=' . $_REQUEST['password'] . '&version=' . $_REQUEST['version']);
